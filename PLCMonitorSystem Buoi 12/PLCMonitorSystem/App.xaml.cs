29fad4682b204a67b3425885bdc3d0becc643e39
QuangLam1996 <quanglam9y@gmail.com>
2025-09-26 16:49:50 +0700

    add .

---
+++ PLCMonitorSystem Buoi 12/PLCMonitorSystem/App.xaml.cs
@@ -0,0 +1,28 @@
+﻿using PLCMonitorSystem.LIB;
+using PLCMonitorSystem.UI;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace PLCMonitorSystem
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        //NonProcedure nonProcedure = new NonProcedure();
+        //Test test = new Test();
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            UIManager.Initial();
+        }
+    }
+}
+
+
---
+++ PLCMonitorSystem Buoi 12/PLCMonitorSystem/LIB/Socket.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using System.Runtime.CompilerServices;
+
+namespace PLCMonitorSystem.LIB
+{
+    public class Ethernet
+    {
+        //Field & Property
+        Socket sock;
+        string ipAddress;
+        int port;
+
+        public string IpAddress { get => ipAddress; set => ipAddress = value; }
+        public int Port { get => port; set => port = value; }
+
+        public Ethernet()
+        {
+            this.IpAddress = "127.0.0.1";
+            this.Port = 6000;
+
+        }
+        public Ethernet(string ipAddress, int port)
+        {
+            this.IpAddress = ipAddress;
+            this.Port = port;
+
+        }
+
+        // Connect
+        public int Connect()
+        {
+            int kq = -1;
+            // B1: Kiểm tra Socket đã khởi tạo
+            if (sock == null)
+            {
+                sock = new Socket(SocketType.Stream, ProtocolType.Tcp);
+            }
+            // B2: Kiểm tra đã kết nối
+            if (sock.Connected == true)
+            {
+                kq = 0;
+                return kq;
+            }
+            // B3: Kết nối
+            try
+            {
+                sock.Connect(this.IpAddress, this.Port);
+                if (sock.Connected == true)
+                {
+                    kq = 0;
+                }
+            }
+            catch (Exception err) { }
+
+            return kq;
+        }
+        public int Close()
+        {
+            int kq = -1;
+            // B1: Kiểm tra Socket đã khởi tạo
+            if (sock == null)
+            {
+                return kq;
+            }
+            if (sock.Connected == false)
+            {
+                return kq;
+            }
+            try
+            {
+                sock.Disconnect(false);
+                sock = null;
+                kq = 0;
+            }
+            catch (Exception err) { }
+
+            return kq;
+        }
+        public int SendData(byte[] data)
+        {
+            int kq = -1;
+            // B1: Kiểm tra đã khởi tạo 
+            if (sock == null)
+            {
+                return kq;
+            }
+            // B2: Kiểm tra kết nối
+            if (sock.Connected == false)
+            {
+                return kq;
+            }
+            // B3: Send
+            try
+            {
+                sock.Send(data);
+                kq = 0;
+            }
+            catch (Exception err) { }
+
+            return kq;
+        }
+        public int RecieveData( out byte[] data)
+        {
+            int kq = -1;
+            // B1: Kiểm tra đã khởi tạo 
+            if (sock == null)
+            {
+                return kq;
+            }
+            // B2: Kiểm tra kết nối
+            if (sock.Connected == false)
+            {
+                return kq;
+            }
+            // B3: Send
+            try
+            {
+                sock.Send(data);
+            }
+            catch (Exception err) { }
+
+            return kq;
+        }
+
+    }
+}
